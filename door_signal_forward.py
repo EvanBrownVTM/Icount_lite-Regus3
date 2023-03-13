1e40c34958e2c8a7b021b950c6de708152df7da0
Evan Brown <evanb@viatouchmedia.com>
2023-03-13 14:40:13 -0700

    video mode

--- door_signal_forward.py
+++ door_signal_forward.py
@@ -4,15 +4,16 @@ import logging
 import pika
 import sys
 import json
+import pickle
 
 #RabbitMQ Initialization
-def initializeChannels(logger, queue_list, ip, credentials = pika.PlainCredentials('nano','nano')):
+def initializeChannels(logger, queue_list, ip, credentials = pika.PlainCredentials('guest','guest')):
 	'''
 		queue_list = ['cvRequest', 'cvPost']
 		ip = 'localhost'
 	'''
 	logger.info('Initializing RMQ queues: ' + ' '.join(queue_list) + ' at : ' + ip)
-	credentials = pika.PlainCredentials('nano','nano')
+	credentials = pika.PlainCredentials('guest','guest')
 	parameters = pika.ConnectionParameters(ip, 5672, '/', credentials, heartbeat=0, blocked_connection_timeout=3000)
 	connection = pika.BlockingConnection(parameters)
 	channel_list = []
@@ -45,18 +46,19 @@ def main():
 	channels, connection = initializeChannels(logger, ['cvIcount'], 'localhost')
 	(channel_icount,) = channels
 
-	#initialize channel to forward door signal to demographics (nano)
+	#initialize channel to forward door signal to demographics (guest)
 	channel_demographics = None
 	start_time = time.time()
 	if cfg.FaceRec:
 		try:
-			channels, connection = initializeChannels(logger, ['cvFace'], cfg.IP_ADDRESS_NANO)
+			channels, connection = initializeChannels(logger, ['cvFace'], cfg.IP_ADDRESS_guest)
 			(channel_demographics,) = channels
 		except Exception as e:
-			logger.info('ERROR: failed to init RMQ channel /door signal to Nano at: ' + cfg.IP_ADDRESS_NANO)
+			logger.info('ERROR: failed to init RMQ channel /door signal to guest at: ' + cfg.IP_ADDRESS_guest)
 			logger.info('   ' + str(e))
 
 	#receive and forward signals
+	transid = None
 	while True:
 		#receive door signal
 		_,_,recv = channel_receive.basic_get('cvRequest')
@@ -67,6 +69,16 @@ def main():
 					routing_key="cvIcount",
 					body=recv
 					)
+			#store load sensor information if postprocess video mode activated
+			if cfg.postprocess_video_mode:
+				recv_json = str(recv,'utf-8')
+				recv_json =json.loads(recv_json)
+				if recv_json["cmd"] == 'DoorOpened':
+					transid = recv_json["parm1"].split(":")[0]
+				if recv_json["cmd"] == "ActivityID":
+					ls_activities = recv_json["parm1"]
+					with open('archive/{}/ls_activities.pickle'.format(transid), 'wb') as f:
+						pickle.dump(ls_activities, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 			#forward message to demographics, if channel exists
 			if channel_demographics is not None:
@@ -76,10 +88,10 @@ def main():
 					)
 			elif (time.time() - start_time) % reconnect_interval == 0 and cfg.FaceRec:
 				try:
-					channels, connection = initializeChannels(logger, ['cvFace'], cfg.IP_ADDRESS_NANO)
+					channels, connection = initializeChannels(logger, ['cvFace'], cfg.IP_ADDRESS_guest)
 					(channel_demographics,) = channels
 				except Exception as e:
-					logger.info('ERROR: failed to init RMQ channel / door signal to Nano at: ' + cfg.IP_ADDRESS_NANO)
+					logger.info('ERROR: failed to init RMQ channel / door signal to guest at: ' + cfg.IP_ADDRESS_guest)
 					logger.info('   ' + str(e))
 
 if __name__ == '__main__':
